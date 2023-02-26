4d514e094692d98276bf4b83e0b9e5828ea96376
MelnikovSG <melnikov-sergey-g@mail.ru>
2023-02-26 16:23:04 +0700

    done

--- src/main/java/com/testing/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
+++ src/main/java/com/testing/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -1,2 +1,15 @@
-package com.testing.spring.mvc_hibernate_aop.dao;public interface EmployeeDAO {
+package com.testing.spring.mvc_hibernate_aop.dao;
+
+import com.testing.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
 }
--- src/main/java/com/testing/spring/mvc_hibernate_aop/service/EmployeeService.java
+++ src/main/java/com/testing/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -1,2 +1,13 @@
-package com.testing.spring.mvc_hibernate_aop.service;public interface EmployeeService {
+package com.testing.spring.mvc_hibernate_aop.service;
+
+import com.testing.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployee(int id);
+    void deleteEmployee(int id);
 }
